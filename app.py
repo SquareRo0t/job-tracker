8cefa8951acf1296d795f685ffe490c9dffd1365
Jacky <jacphu-7@student.ltu.sem>
2026-07-06 23:03:51 +0200

    upate8

--- app.py
+++ app.py
@@ -111,15 +111,46 @@ def update_job(job_id):
 
 # ----------------------------------------------------------------
 
-@app.route("/edit/<int:job_id>")
+# @app.route("/edit/<int:job_id>")
+# def edit_job(job_id):
+#     # Hämta ansökan med matchande id från databasen 
+#     with sqlite3.connect(DB_PATH) as conn:
+#         cursor6 = conn.cursor()
+#         # fetchone() hämtar bara en rad istället för alla
+#         job = cursor6.execute("SELECT * FROM JOB WHERE id = ?",(job_id,)).fetchone()
+#      # Skicka radens data till redigeringssidan
+#     return render_template("edit.html", job=job)
+
+# ----------------------------------------------------------------
+
+@app.route("/edit/<int:job_id>", methods=["GET","POST"])
 def edit_job(job_id):
-    # Hämta ansökan med matchande id från databasen 
-    with sqlite3.connect(DB_PATH) as conn:
-        cursor6 = conn.cursor()
-        # fetchone() hämtar bara en rad istället för alla
-        job = cursor6.execute("SELECT * FROM JOB WHERE id = ?",(job_id,)).fetchone()
-     # Skicka radens data till redigeringssidan
-    return render_template("edit.html", job=job)
+
+    if request.method == "POST":
+        
+        company = request.form["company"]
+        date = request.form["date"]
+        recruites_name = request.form["recruite_name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        status = request.form["status"]
+    
+        # Hämta ansökan med matchande id från databasen 
+        with sqlite3.connect(DB_PATH) as conn:
+            cursor6 = conn.cursor()
+            cursor6.execute("UPDATE JOB SET company = ?, date_applied = ?, recruiter_name = ?, recruiter_email = ?, recruiter_phone = ?, status = ? WHERE id = ?",(company, date, recruites_name, email, phone, status, job_id,))
+            conn.commit()
+    else:
+        # Hämta ansökan med matchande id från databasen 
+        with sqlite3.connect(DB_PATH) as conn:
+            cursor6 = conn.cursor()
+            # fetchone() hämtar bara en rad istället för alla
+            job = cursor6.execute("SELECT * FROM JOB WHERE id = ?",(job_id,)).fetchone()
+        # Skicka radens data till redigeringssidan
+        return render_template("edit.html", job=job)
+
+    # Skicka tillbaka användaren till startsidan
+    return redirect("/")
 
 # ----------------------------------------------------------------
 
